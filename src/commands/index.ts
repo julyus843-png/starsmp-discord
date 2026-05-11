e574b274c4875721e32327b5f58e9c6c515c7993
julyus843-png <julyus843@gmail.com>
2026-05-11 11:35:28 -0400

    Add src/commands/index.ts

---
+++ src/commands/index.ts
@@ -0,0 +1,8 @@
+import { ban } from "./ban.js";
+import { tempban } from "./tempban.js";
+import { mute } from "./mute.js";
+import { pollcreate } from "./pollcreate.js";
+import { embed } from "./embed.js";
+import { invite } from "./invite.js";
+
+export const commands = [ban, tempban, mute, pollcreate, embed, invite];
